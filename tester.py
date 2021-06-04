5a956a9ba410c74e1eed905543cf2b4c65321197
Seo-Rim Hwang <55497506+seorim0@users.noreply.github.com>
2021-06-04 15:23:35 +0900

    Update tester.py

--- tester.py
+++ tester.py
@@ -4,11 +4,13 @@ from tools_for_estimate import cal_pesq, cal_stoi, composite
 import config as cfg
 from scipy.io.wavfile import write as wav_write
 
+file_save = False
+
 
 #######################################################################
 #                           For evaluation                            #
 #######################################################################
-def model_test(model, validation_loader, noise_type, snr, direct, dir_to_save, epoch, DEVICE):
+def model_test(noise_type, snr, model, validation_loader, direct, dir_to_save, epoch, DEVICE):
     # initialize
     batch_num = 0
 
@@ -25,7 +27,7 @@ def model_test(model, validation_loader, noise_type, snr, direct, dir_to_save, e
     all_batch_stoi = []
 
     # for record the score each samples
-    f_score = open(dir_to_save + '/Epoch_' + '%d_SCORES' % epoch, 'a')
+    f_score = open(dir_to_save + '/Epoch_' + '%s_SCORES' % epoch, 'a')
     for inputs, targets in tools.Bar(validation_loader):
         batch_num += 1
 
@@ -33,7 +35,10 @@ def model_test(model, validation_loader, noise_type, snr, direct, dir_to_save, e
         inputs = inputs.float().to(DEVICE)
         targets = targets.float().to(DEVICE)
 
-        outputs = model(inputs, direct)
+        _, _, outputs = model(inputs, direct)
+
+        if len(outputs[0]) < len(targets[0]):
+            targets = targets[:, :len(outputs[0])]
 
         # estimate the output speech with pesq and stoi
         estimated_wavs = outputs.cpu().detach().numpy()
@@ -46,39 +51,41 @@ def model_test(model, validation_loader, noise_type, snr, direct, dir_to_save, e
         pesq = np.reshape(pesq, (1, -1))
         stoi = np.reshape(stoi, (1, -1))
 
-        # all batch data array
-        all_batch_input.extend(inputs)
-        all_batch_target.extend(targets)
-        all_batch_output.extend(outputs)
-        all_batch_pesq.extend(pesq[0])
-        all_batch_stoi.extend(stoi[0])
+        if file_save:
+            # all batch data array
+            all_batch_input.extend(inputs)
+            all_batch_target.extend(targets)
+            all_batch_output.extend(outputs)
+            all_batch_pesq.extend(pesq[0])
+            all_batch_stoi.extend(stoi[0])
 
         avg_pesq += sum(pesq[0]) / len(inputs)
         avg_stoi += sum(stoi[0]) / len(inputs)
 
-    estfile_path = './output/{}/{}dB/'.format(noise_type, snr)
-    for m in range(len(all_batch_output)):
-        est_file_name = '{}dB_{}_est_{}_{:.5}.wav'.format(snr, m + 1, all_batch_pesq[m])
+    if file_save:
+        estfile_path = './output/{}/{}dB/'.format(noise_type, snr)
+        for m in range(len(all_batch_output)):
+            est_file_name = '{}dB_{}_est_{}_{:.5}.wav'.format(snr, m + 1, all_batch_pesq[m])
 
-        est_wav = all_batch_output[m].cpu().detach().numpy()
-        wav_write(estfile_path+est_file_name, cfg.fs, est_wav)
+            est_wav = all_batch_output[m].cpu().detach().numpy()
+            wav_write(estfile_path+est_file_name, cfg.fs, est_wav)
 
-        noisy_file_name = '{}dB/{}dB_{}_noisy_{:.5}.wav'.format(snr, m + 1, all_batch_input[m])
-        noisy_wav = all_batch_input[m].cpu().detach().numpy()
-        wav_write(estfile_path+noisy_file_name, cfg.fs, noisy_wav)
+            noisy_file_name = '{}dB/{}dB_{}_noisy_{:.5}.wav'.format(snr, m + 1, all_batch_input[m])
+            noisy_wav = all_batch_input[m].cpu().detach().numpy()
+            wav_write(estfile_path+noisy_file_name, cfg.fs, noisy_wav)
 
-        clean_file_name = '{}dB_{}_clean.wav'.format(snr, m + 1)
-        clean_wav = all_batch_target[m].cpu().detach().numpy()
-        wav_write(estfile_path+clean_file_name, cfg.fs, clean_wav)
+            clean_file_name = '{}dB_{}_clean.wav'.format(snr, m + 1)
+            clean_wav = all_batch_target[m].cpu().detach().numpy()
+            wav_write(estfile_path+clean_file_name, cfg.fs, clean_wav)
 
-        CSIG, CBAK, CVOL, _ = composite(estfile_path+clean_file_name, estfile_path+est_file_name)
-        avg_csig += CSIG
-        avg_cbak += CBAK
-        avg_cvol += CVOL
+            CSIG, CBAK, CVOL, _ = composite(estfile_path+clean_file_name, estfile_path+est_file_name)
+            avg_csig += CSIG
+            avg_cbak += CBAK
+            avg_cvol += CVOL
 
-        # pesq: 0.1 better / stoi: 0.01 better
-        f_score.write('PESQ {:.6f} | STOI {:.6f} | CSIG {:.6f} | CBAK {:.6f} | CVOL {:.6f}\n'
-                          .format(all_batch_pesq[m], all_batch_stoi[m], CSIG, CBAK, CVOL))
+            # pesq: 0.1 better / stoi: 0.01 better
+            f_score.write('PESQ {:.6f} | STOI {:.6f} | CSIG {:.6f} | CBAK {:.6f} | CVOL {:.6f}\n'
+                              .format(all_batch_pesq[m], all_batch_stoi[m], CSIG, CBAK, CVOL))
 
     avg_pesq /= batch_num
     avg_stoi /= batch_num
