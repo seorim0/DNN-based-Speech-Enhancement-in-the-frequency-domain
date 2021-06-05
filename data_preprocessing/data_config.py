377d7a34622f0f4b3dac22d61969fbc3f78c15d8
Seo-Rim Hwang <55497506+seorim0@users.noreply.github.com>
2021-06-05 11:11:16 +0900

    Update data_config.py

--- data_preprocessing/data_config.py
+++ data_preprocessing/data_config.py
@@ -6,17 +6,30 @@ for data preprocessing
 fs = 16000
 snr_set = [0, 5, 10, 15, 20]
 
-data_name = ""
+data_name = "C1"
 mode = "train"  # train | validation | test
 
 noise_subset = ['chime2_lounge', 'chime3_bus', 'chime3_cafe', 'chime3_pedestrain', 'chime3_street',
                           'noisex92_babble', 'noisex92_factory', 'noisex92_military',
                           'noisex92_pink', 'noisex92_volvo', 'noisex92_white']
-# ['etsi_inside_aircraft', 'etsi_kindergarten', 'etsi_mensa', 'etsi_schoolyard',
-#  'etsi_shopping_center', 'etsi_train_station', 'tank']
 
 # for test setup
 padding = True
 test_noise_type = "seen"  # seen | unseen
 
+# for pam
+pam = True
 
+win_len = 400
+win_inc = 100
+ola_ratio = win_inc / win_len
+fft_len = 512
+window = 'hanning'
+
+
+# [-20, -15, -10, -5, 0]
+# ['chime2_lounge', 'chime3_bus', 'chime3_cafe', 'chime3_pedestrain', 'chime3_street',
+#                           'noisex92_babble', 'noisex92_factory', 'noisex92_military',
+#                           'noisex92_pink', 'noisex92_volvo', 'noisex92_white']
+# ['etsi_inside_aircraft', 'etsi_kindergarten', 'etsi_mensa', 'etsi_schoolyard',
+#  'etsi_shopping_center', 'etsi_train_station', 'tank']
