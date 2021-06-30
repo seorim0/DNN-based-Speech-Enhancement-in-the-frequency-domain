9b315d5df011c3f465c09e5eed73cb64e092015e
Seo-Rim Hwang <55497506+seorim0@users.noreply.github.com>
2021-06-30 16:43:34 +0900

    Update dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -3,6 +3,9 @@ import numpy as np
 from torch.utils.data import Dataset, DataLoader
 import config as cfg
 
+np_load_old = np.load
+np.load = lambda *a, **k: np_load_old(*a, allow_pickle=True, **k)
+
 
 def create_dataloader(mode, type=0, snr=0):
     if mode == 'train':
@@ -33,12 +36,12 @@ class Wave_Dataset(Dataset):
         if mode == 'train':
             print('<Training dataset>')
             print('Load the data...')
-            self.input_path = './input/C1_train_dataset.npy'
+            self.input_path = './input/PAM_C1+train_dataset.npy'
             self.input = np.load(self.input_path)
         elif mode == 'valid':
             print('<Validation dataset>')
             print('Load the data...')
-            self.input_path = './input/C1_validation_dataset.npy'
+            self.input_path = './input/PAM_C1+validation_dataset.npy'
             self.input = np.load(self.input_path)
 
             # # if you want to use a part of the dataset
@@ -55,11 +58,23 @@ class Wave_Dataset(Dataset):
         return len(self.input)
 
     def __getitem__(self, idx):
-        inputs = self.input[idx][0]
-        targets = self.input[idx][1]
+        if cfg.perceptual == 'PAM':
+            inputs = self.input[idx][0]
+            targets = self.input[idx][1][0]
+            GMTs = self.input[idx][1][1]
 
-        # transform to torch from numpy
-        inputs = torch.from_numpy(inputs)
-        targets = torch.from_numpy(targets)
+            # transform to torch from numpy
+            inputs = torch.from_numpy(inputs)
+            targets = torch.from_numpy(targets)
+            GMTs = torch.from_numpy(GMTs)
 
-        return inputs, targets
+            return inputs, targets, GMTs
+        else:
+            inputs = self.input[idx][0]
+            targets = self.input[idx][1]
+
+            # transform to torch from numpy
+            inputs = torch.from_numpy(inputs)
+            targets = torch.from_numpy(targets)
+
+            return inputs, targets
