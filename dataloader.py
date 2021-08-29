a127db75c65690a710b4b89bd0efb4d7573ff8d3
Seo-Rim Hwang <55497506+seorim0@users.noreply.github.com>
2021-08-29 18:47:53 +0900

    Update dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -37,13 +37,18 @@ class Wave_Dataset(Dataset):
             self.mode = 'train'
             print('<Training dataset>')
             print('Load the data...')
-            self.input_path = './input/PAM_C1+_v2_train_dataset.npy'
-            self.input = np.load(self.input_path)
+            # self.input_path = './input/to_Noisy_train_dataset.npy'
+            # self.input = np.load(self.input_path)
+            self.input = []
+            for i in range(1, 3):
+                self.current_input_path = './input/to_Noisy_train_dataset{}.npy'.format(i)
+                self.current_input = np.load(self.current_input_path)
+                self.input.extend(self.current_input)
         elif mode == 'valid':
             self.mode = 'valid'
             print('<Validation dataset>')
             print('Load the data...')
-            self.input_path = './input/PAM_C1+_v2_validation_dataset.npy'
+            self.input_path = './input/to_Noisy_validation_dataset.npy'
             self.input = np.load(self.input_path)
 
             # # if you want to use a part of the dataset
@@ -52,7 +57,7 @@ class Wave_Dataset(Dataset):
             self.mode = 'test'
             print('<Test dataset>')
             print('Load the data...')
-            self.input_path = './input/C1+_v2_test_dataset.npy'
+            self.input_path = './input/MS_29m_test_dataset.npy'
 
             self.input = np.load(self.input_path)
             self.input = self.input[type][snr]
@@ -72,6 +77,37 @@ class Wave_Dataset(Dataset):
             GMTs = torch.from_numpy(GMTs)
 
             return inputs, targets, GMTs
+        elif cfg.conditional_learning:
+            # conditions = self.input[idx][0]
+            # inputs = self.input[idx][1]
+            # targets = self.input[idx][2]
+            #
+            # # transform to torch from numpy
+            # conditions = torch.from_numpy(conditions)
+            # inputs = torch.from_numpy(inputs)
+            # targets = torch.from_numpy(targets)
+            #
+            # return conditions, inputs, targets
+            if self.mode == 'train':
+                noisy_conditions = self.input[idx][3]  # 2: 10dB, 3: 20dB, 4: 30dB, 5: 40dB
+                inputs = self.input[idx][0]
+                targets = self.input[idx][1]
+
+                # transform to torch from numpy
+                noisy_conditions = torch.from_numpy(noisy_conditions)
+                inputs = torch.from_numpy(inputs)
+                targets = torch.from_numpy(targets)
+
+                return noisy_conditions, inputs, targets
+            if self.mode == 'valid':
+                inputs = self.input[idx][0]
+                targets = self.input[idx][1]
+
+                # transform to torch from numpy
+                inputs = torch.from_numpy(inputs)
+                targets = torch.from_numpy(targets)
+
+                return inputs, targets
         else:
             inputs = self.input[idx][0]
             targets = self.input[idx][1]
@@ -81,3 +117,4 @@ class Wave_Dataset(Dataset):
             targets = torch.from_numpy(targets)
 
             return inputs, targets
+
