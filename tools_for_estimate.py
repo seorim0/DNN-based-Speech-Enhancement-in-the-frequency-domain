f54fe41ea7eddb5ed8a2853b31fcf8fa4dae6a45
Seo-Rim Hwang <55497506+seorim0@users.noreply.github.com>
2021-08-31 11:39:48 +0900

    Update tools_for_estimate.py

--- tools_for_estimate.py
+++ tools_for_estimate.py
@@ -9,6 +9,26 @@ import config as cfg
 
 
 ############################################################################
+#                                   MOS                                    #
+############################################################################
+# Reference 
+# https://github.com/usimarit/semetrics # https://ecs.utdallas.edu/loizou/speech/software.htm
+logging.basicConfig(level=logging.ERROR)
+oc = oct2py.Oct2Py(logger=logging.getLogger())
+
+COMPOSITE = os.path.join(os.path.abspath(os.path.dirname(__file__)), "composite.m")
+
+
+def composite(clean: str, enhanced: str):
+    pesq_score = pesq_mos(clean, enhanced)
+    csig, cbak, covl, ssnr = oc.feval(COMPOSITE, clean, enhanced, nout=4)
+    csig += 0.603 * pesq_score
+    cbak += 0.478 * pesq_score
+    covl += 0.805 * pesq_score
+    return csig, cbak, covl, ssnr
+
+
+############################################################################
 #                                   PESQ                                   #
 ############################################################################
 # Reference
