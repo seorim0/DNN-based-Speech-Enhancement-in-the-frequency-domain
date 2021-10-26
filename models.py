a462e89c9a7c224211ed931ee259f62020cb3802
Seo-Rim Hwang <55497506+seorim0@users.noreply.github.com>
2021-10-26 21:48:31 +0900

    Update models.py

--- models.py
+++ models.py
@@ -561,3 +561,123 @@ class CRN(nn.Module):
                 return -(si_snr(estimated, target))
             elif cfg.loss == 'SI-SDR':
                 return -(si_sdr(target, estimated))
+
+            
+class FullSubNet(BaseModel):
+    def __init__(self,
+                 sb_num_neighbors=cfg.sb_num_neighbors,
+                 fb_num_neighbors=cfg.fb_num_neighbors,
+                 num_freqs=cfg.num_freqs,
+                 look_ahead=cfg.look_ahead,
+                 sequence_model=cfg.sequence_model,
+                 fb_output_activate_function=cfg.fb_output_activate_function,
+                 sb_output_activate_function=cfg.sb_output_activate_function,
+                 fb_model_hidden_size=cfg.fb_model_hidden_size,
+                 sb_model_hidden_size=cfg.sb_model_hidden_size,
+                 weight_init=cfg.weight_init,
+                 norm_type=cfg.norm_type,
+                 num_groups_in_drop_band=cfg.num_groups_in_drop_band,
+                 ):
+        """
+        FullSubNet model (cIRM mask)
+
+        Args:
+            num_freqs: Frequency dim of the input
+            look_ahead: Number of use of the future frames
+            fb_num_neighbors: How much neighbor frequencies at each side from fullband model's output
+            sb_num_neighbors: How much neighbor frequencies at each side from noisy spectrogram
+            sequence_model: Chose one sequence model as the basic model e.g., GRU, LSTM
+            fb_output_activate_function: fullband model's activation function
+            sb_output_activate_function: subband model's activation function
+            norm_type: type of normalization, see more details in "BaseModel" class
+        """
+        super().__init__()
+        assert sequence_model in ("GRU", "LSTM"), f"{self.__class__.__name__} only support GRU and LSTM."
+
+        self.fb_model = SequenceModel(
+            input_size=num_freqs,
+            output_size=num_freqs,
+            hidden_size=fb_model_hidden_size,
+            num_layers=2,
+            bidirectional=False,
+            sequence_model=sequence_model,
+            output_activate_function=fb_output_activate_function
+        )
+
+        self.sb_model = SequenceModel(
+            input_size=(sb_num_neighbors * 2 + 1) + (fb_num_neighbors * 2 + 1),
+            output_size=2,
+            hidden_size=sb_model_hidden_size,
+            num_layers=2,
+            bidirectional=False,
+            sequence_model=sequence_model,
+            output_activate_function=sb_output_activate_function
+        )
+
+        self.sb_num_neighbors = sb_num_neighbors
+        self.fb_num_neighbors = fb_num_neighbors
+        self.look_ahead = look_ahead
+        self.norm = self.norm_wrapper(norm_type)
+        self.num_groups_in_drop_band = num_groups_in_drop_band
+
+        if weight_init:
+            self.apply(self.weight_init)
+
+    def forward(self, noisy_mag):
+        """
+        Args:
+            noisy_mag: noisy magnitude spectrogram
+
+        Returns:
+            The real part and imag part of the enhanced spectrogram
+
+        Shapes:
+            noisy_mag: [B, 1, F, T]
+            return: [B, 2, F, T]
+        """
+        if not noisy_mag.dim() == 4:
+            noisy_mag = noisy_mag.unsqueeze(1)
+        noisy_mag = functional.pad(noisy_mag, [0, self.look_ahead])  # Pad the look ahead
+        batch_size, num_channels, num_freqs, num_frames = noisy_mag.size()
+        assert num_channels == 1, f"{self.__class__.__name__} takes the mag feature as inputs."
+
+        # Fullband model
+        fb_input = self.norm(noisy_mag).reshape(batch_size, num_channels * num_freqs, num_frames)
+        fb_output = self.fb_model(fb_input).reshape(batch_size, 1, num_freqs, num_frames)
+
+        # Unfold fullband model's output, [B, N=F, C, F_f, T]. N is the number of sub-band units
+        fb_output_unfolded = self.unfold(fb_output, num_neighbor=self.fb_num_neighbors)
+        fb_output_unfolded = fb_output_unfolded.reshape(batch_size, num_freqs, self.fb_num_neighbors * 2 + 1, num_frames)
+
+        # Unfold noisy spectrogram, [B, N=F, C, F_s, T]
+        noisy_mag_unfolded = self.unfold(noisy_mag, num_neighbor=self.sb_num_neighbors)
+        noisy_mag_unfolded = noisy_mag_unfolded.reshape(batch_size, num_freqs, self.sb_num_neighbors * 2 + 1, num_frames)
+
+        # Concatenation, [B, F, (F_s + F_f), T]
+        sb_input = torch.cat([noisy_mag_unfolded, fb_output_unfolded], dim=2)
+        sb_input = self.norm(sb_input)
+
+        sb_input = sb_input.reshape(
+            batch_size * num_freqs,
+            (self.sb_num_neighbors * 2 + 1) + (self.fb_num_neighbors * 2 + 1),
+            num_frames
+        )
+
+        # [B * F, (F_s + F_f), T] => [B * F, 2, T] => [B, F, 2, T]
+        sb_mask = self.sb_model(sb_input)
+        sb_mask = sb_mask.reshape(batch_size, num_freqs, 2, num_frames).permute(0, 2, 1, 3).contiguous()
+
+        output = sb_mask[:, :, :, self.look_ahead:]
+        output = output.permute(0, 2, 3, 1)
+        return output
+
+    def loss(self, estimated, target):
+            if cfg.loss == 'MSE':
+                return functional.mse_loss(estimated, target, reduction='mean')
+            elif cfg.loss == 'SDR':
+                return -sdr(target, estimated)
+            elif cfg.loss == 'SI-SNR':
+                return -(si_snr(estimated, target))
+            elif cfg.loss == 'SI-SDR':
+                return -(si_sdr(target, estimated))
+            
